c2e835f9551badaf2ed62edd21cd9cf09ce2a1e3
Azazlokus <62481032+Azazlokus@users.noreply.github.com>
2023-06-16 22:11:38 +0700

    init commit

---
+++ app/Http/Controllers/PlaceController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PlaceController extends Controller
+{
+    public function getPlaces(): string
+    {
+        return 'sf';
+    }
+    public function addPlace(){
+
+    }
+    //
+}
---
+++ routes/api.php
@@ -0,0 +1,28 @@
+<?php
+
+use App\Http\Controllers\PlaceController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "api" middleware group. Make something great!
+|
+*/
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
+Route::get('/getPlaces', [PlaceController::class, 'getPlaces']);
+
+
+Route::post('/tokens/create', function (Request $request) {
+    $token = $request->user()->createToken($request->token_name);
+
+    return ['token' => $token->plainTextToken];
+});
